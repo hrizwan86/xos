b4914c0acb04927b40754abee6ec136cc62ec167
Rizwan Haider <hrizwan@hrizwan.lra.lucent.com>
2016-04-05 14:49:53 -0700

    ExampleService created

--- xos/xos/settings.py
+++ xos/xos/settings.py
@@ -175,6 +175,7 @@ INSTALLED_APPS = (
     'core',
     'services.hpc',
     'services.cord',
+    'services.ecord',
     'services.helloworldservice_complete',
     'services.onos',
     'services.ceilometer',
